7999b823b86d55fd32b563c645278037221422b6
pedro-jgl <pedrojgl14@gmail.com>
2021-03-15 19:15:07 +0100

    Creado principal

---
+++ src/Principal.rb
@@ -0,0 +1,17 @@
+# encoding: utf-8
+
+=begin
+require 'Loot' 
+require 
+require 'Dice'
+require 'GameCharacter'
+require 'shieldBooster'
+require 'ShotResult'
+require 'SuppliesPackage'
+require 'Weapon'
+require 'WeaponType'
+=end
+
+puts "Hello"
+dado = Dice.new()
+puts dado.inspect
